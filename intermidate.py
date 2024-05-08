17d778ea5020219f7f9518bd95b06b45f190037e
Gravtas-J <144185093+Gravtas-J@users.noreply.github.com>
2024-05-08 14:02:59 +1000

    Reverted back, memory doesn't work the same as the library

--- intermidate.py
+++ intermidate.py
@@ -8,7 +8,8 @@ import time
 # This app is for those who         #
 # are running an ollama instance    #
 # locally or on your network.       #
-#                                   #
+# There is Currently no memory of   #
+# chat                              #
 #####################################
 
 model = "phi3"  
@@ -41,7 +42,6 @@ def chat(messages):
         )
         r.raise_for_status()
         output = ""
-        
         for line in r.iter_lines():
             body = json.loads(line)
             if "error" in body:
@@ -57,20 +57,24 @@ def chat(messages):
         return {"content": str(e)}
 
 def main():
-    st.title("Ollama Chat Interface")
+    st.title("LLaMA Chat Interface")
     user_input = st.chat_input("Enter your prompt:", key="1")
     if 'messages' not in st.session_state:
         st.session_state['messages'] = []
     show_msgs()
     if user_input:
-        with st.chat_message("user"):
-            st.write(user_input)
-        st.session_state.messages.append({"role": "user", "content": user_input})
-        messages = "\n".join(msg["content"] for msg in st.session_state.messages)
-        response = chat(messages)
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        with st.chat_message("assistant"):
-            st.write_stream(response_generator(response))
+            with st.chat_message("user",):
+                st.write(user_input)
+            messages = [{"role": "user", "content": user_input}]
+            #TODO This works when using the library but not with the server with requests. Dunno why
+            # messages = "\n".join(msg["content"] for msg in st.session_state.messages)
+            # response = chat(messages)
+            st.session_state.messages.append({"role": "user", "content": user_input})
+            response = chat(messages)
+            st.session_state.messages.append({"role": "assistant", "content": (response.get("content", "")), })
+            # st.chat_message("bot").write(response.get("content", ""))
+            with st.chat_message("assistant"):
+                st.write_stream(response_generator(response.get("content", "")))    
     else:
         st.info("Enter a prompt above to start the conversation")
 
